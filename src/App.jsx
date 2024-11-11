1d4b423e37f752802d151fcc6cfd041e32d8911f
vishu-bhandari <deevishu1234@gmail.com>
2024-11-11 05:48:09 +0530

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="text-center p-10 text-2xl font-bold text-blue-500">
+      Welcome to My Landing Page!
+    </div>
+  );
+}
